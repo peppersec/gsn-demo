f49b05ccd5858929f2d54a01541b2f040502aa02
Alexey <peretsmobil@gmail.com>
2019-09-24 19:22:42 +0300

    withdrawViaRelayer test

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -9,6 +9,13 @@ contract Counter is GSNRecipient {
     value += 1;
   }
 
+  event Withdraw(address who);
+  event PostRelayCall(uint actualCharge, bytes context, address recipient);
+
+  function withdrawViaRelayer(uint256[2] memory a, uint256[2][2] memory b, uint256[2] memory c, uint256[3] memory input) public {
+    emit Withdraw(address(input[2]));
+  }
+
   function acceptRelayedCall(
     address relay,
     address from,
@@ -21,5 +28,23 @@ contract Counter is GSNRecipient {
     uint256 maxPossibleCharge
   ) external view returns (uint256, bytes memory) {
     return _approveRelayedCall();
+  }
+
+  // this func is called by RelayerHub right before calling a target func
+  function preRelayedCall(bytes calldata /*context*/) external returns (bytes32) {}
+
+
+  // this func is called by RelayerHub right after calling a target func
+  function postRelayedCall(bytes memory context, bool /*success*/, uint actualCharge, bytes32 /*preRetVal*/) public {
+    // IRelayHub relayHub = IRelayHub(getHubAddr());
+    address payable recipient;
+    assembly {
+      recipient := sload(add(context, 324)) // 4 + (8 * 32) + (32) + (32) == selector + proof + root + nullifier
+    }
+    emit PostRelayCall(actualCharge, context, recipient);
+
+    // recipient.transfer(mixDenomination - actualCharge);
+    // relayHub.depositFor.value(actualCharge)(address(this));
+    // or we can send actualCharge somewhere else...
   }
 }
--- test/Counter.test.js
+++ test/Counter.test.js
@@ -57,10 +57,41 @@ contract('Counter', accounts => {
                 verbose: true
             })
             const web3 = new Web3(provider)
-            counter = new web3.eth.Contract(counter.abi, counter.address)  
-            await counter.methods.increase().send({ from: account.address })
-            const newValue = await counter.methods.value().call()
+            gsnCounter = new web3.eth.Contract(counter.abi, counter.address)  
+            await gsnCounter.methods.increase().send({ from: account.address })
+            const newValue = await counter.value()
             newValue.should.be.eq.BN(value.add(toBN(1)))
+        })
+    })
+
+    describe('#withdrawViaRelayer', () => {
+        it('should work', async () => {
+            const account = ephemeral()
+            const provider = new GSNDevProvider("http://localhost:8545", { 
+                signKey: account, 
+                ownerAddress: accounts[8],
+                relayerAddress: accounts[9],
+                verbose: true
+            })
+            const web3 = new Web3(provider)
+            gsnCounter = new web3.eth.Contract(counter.abi, counter.address)
+            const { pi_a, pi_b, pi_c, publicSignals } = { pi_a:
+                [ '0x25ab09606599b84787eff63bbcc7fa091de5b5e724ded92909c7507b940a0b01',
+                  '0x0fe3c2a5c1af4b68de7fb89e6b7d7bbe8d4624f5dbe41f25a1bc1b649666f548' ],
+               pi_b:
+                [ [ '0x1151b1c499d0237e41c4be108d25aea9d63ce3fd2f0ff728cf1d901dc955374c',
+                    '0x0c8e0bbfe6864dd682c670e0c18a00c082bd45c2b82c3fb84de782f09a29c87f' ],
+                  [ '0x238d84dc3686977a4efcb7aa76d3410da5cf13d198347e3fc5e9407dde6b6614',
+                    '0x1dc676a135cecf39accb0138120aecb2de51f715c5abd94ef0fcc39c3b2a953d' ] ],
+               pi_c:
+                [ '0x21e3ea378f13ca7413df425348a0c71d7081d0f2f42bce8e19ff56e44d958507',
+                  '0x1c559a568f288cad1a5a6f9b4ef1ccda3adae40a490253b83a358f429dd2f695' ],
+               publicSignals:
+                [ '0x15163f70d3c051ca52c44727793f2110f0fb032c62558bb97dc4d806203e39c2',
+                  '0x2f9ca4dbaa092015f21cf0625eb46fbf4a00dff94f8a2988287567560eb0f5f0',
+                  '0x000000000000000000000000972c2188db25a0ca95e2d000a129fb9d2bba4e2c' ] }
+            const tx = await gsnCounter.methods.withdrawViaRelayer(pi_a, pi_b, pi_c, publicSignals).send({ from: account.address })
+            console.log('tx', tx)
         })
     })
 
